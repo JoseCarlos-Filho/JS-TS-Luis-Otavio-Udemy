7dffac39d6a57e6e508486ac539696578459b4a4
José Carlos <etijcvivi@gmail.com>
2022-09-15 20:04:08 -0300

    JavaScript - TypeScript - Luis Otavio - Aula Continue e Break

---
+++ JavaScript_TypeScript/JS/Exercicio de lógica/funcaoModoPaisagem.js
@@ -0,0 +1,11 @@
+// function ePaisagem(largura, altura) {
+//   return largura > altura ? true : false; // condição ternária
+// }
+
+// console.log(ePaisagem(100, 200));
+
+// const ePaisagem = (largura, altura) => largura > altura ? true : false;
+// console.log(ePaisagem(200, 100));
+
+const imgPaisagem = (largura, altura) => largura > altura;
+console.log(imgPaisagem(100, 200));

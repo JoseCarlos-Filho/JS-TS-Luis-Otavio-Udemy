3eeeda71c7c6dbd0641e97f2b53e951e62de85ba
José Carlos <etijcvivi@gmail.com>
2022-09-22 15:48:58 -0300

    JavaScript - TypeScript - Luis Otavio - setInterval e setTimeout

--- JavaScript_TypeScript/JS/Aula46/setIntervalsetTimeOut.js
+++ JavaScript_TypeScript/JS/Aula46/setIntervalsetTimeOut.js
@@ -6,5 +6,21 @@ function viewHours() {
   });
 }
 
-// Retorna
-setInterval();
+// Arrow function
+// setInterval(() => {
+//   console.log(viewHours());
+// }, 1000);
+
+// Outra solução
+const timer = setInterval(() => {
+  console.log(viewHours());
+}, 1000);
+
+// função que para a execução em um determinado intervalo
+setTimeout(() => {
+  clearInterval(timer); // para a execução
+}, 10000);
+
+setTimeout(() => {
+  console.log("Execução finalizada!!!");
+}, 12000);

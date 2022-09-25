1ab36445003f55beeb1b153ba8bbc5ded411052f
José Carlos <etijcvivi@gmail.com>
2022-09-25 13:40:16 -0300

    JavaScript - TypeScript - Jose Carlos - Lista de tarefas

--- JavaScript_TypeScript/JS/Aula 48 - Lista de Tarefas/assets/js/main.js
+++ JavaScript_TypeScript/JS/Aula 48 - Lista de Tarefas/assets/js/main.js
@@ -2,7 +2,17 @@ const inputTarefas = document.querySelector(".input_tarefas");
 const btnTarefas = document.querySelector(".btn_tarefas");
 const listaTarefas = document.querySelector(".lista_tarefas");
 
+function criaTagLi() {
+  const li = document.createElement("li");
+  return li;
+}
+
+function criaTarefa(textoInput) {
+  // console.log(textoInput);
+}
+
 btnTarefas.addEventListener("click", () => {
   if (!inputTarefas.value) return;
-  console.log(inputTarefas.value);
+  // console.log(inputTarefas.value);
+  criaTarefa(inputTarefas.value);
 });

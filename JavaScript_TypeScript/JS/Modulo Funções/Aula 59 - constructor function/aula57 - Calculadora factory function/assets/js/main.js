fc146234f8e385169f720853e795b17092fc0bf8
José Carlos <etijcvivi@gmail.com>
2022-10-24 16:04:51 -0300

    JavaScript - TypeScript - Jose Carlos: Calculadora Funções Construtoras

--- JavaScript_TypeScript/JS/Modulo Funções/Aula 59 - constructor function/aula57 - Calculadora factory function/assets/js/main.js
+++ JavaScript_TypeScript/JS/Modulo Funções/Aula 59 - constructor function/aula57 - Calculadora factory function/assets/js/main.js
@@ -6,9 +6,14 @@ function Calculadora() {
     document.addEventListener("click", (event) => {
       const el = event.target;
       console.log(el.classList);
-      if (el.classList.contains("btn-num")) this.addNumDisplay();
+      if (el.classList.contains("btn-num")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-clear")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-del")) this.addNumDisplay(el);
+      if (el.classList.contains("btn-eq")) this.addNumDisplay(el);
     });
   };
+
+  this.addNumDisplay = (el) => (this.display.value += el.innerText);
 
   this.inicia = () => {
     this.capturaClick();

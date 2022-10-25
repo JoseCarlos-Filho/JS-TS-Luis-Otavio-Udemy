e0bd184207a35cec6594454ec514557dc5d4a82f
José Carlos <etijcvivi@gmail.com>
2022-10-25 12:09:06 -0300

    JavaScript TypeScript - José Carlos: Calculadora funções construtoras

--- JavaScript_TypeScript/JS/Modulo Funções/Aula 59 - constructor function/aula57 - Calculadora factory function/assets/js/main.js
+++ JavaScript_TypeScript/JS/Modulo Funções/Aula 59 - constructor function/aula57 - Calculadora factory function/assets/js/main.js
@@ -2,14 +2,25 @@
 function Calculadora() {
   this.display = document.querySelector(".display");
 
+  this.inicia = () => {
+    this.capturaClick();
+    this.capturaEnter();
+  };
+
+  this.capturaEnter = () => {
+    this.display.addEventListener("keypress", (e) => {
+      if (e.keyCode !== 13) return;
+      this.resultado();
+    });
+  };
   this.capturaClick = () => {
     document.addEventListener("click", (event) => {
       const el = event.target;
-      console.log(el.classList);
+      // console.log(el.classList);
       if (el.classList.contains("btn-num")) this.addNumDisplay(el);
       if (el.classList.contains("btn-clear")) this.clear();
-      if (el.classList.contains("btn-del")) this.del(el);
-      if (el.classList.contains("btn-eq")) this.result(el);
+      if (el.classList.contains("btn-del")) this.del();
+      if (el.classList.contains("btn-eq")) this.resultado();
     });
   };
 
@@ -17,9 +28,22 @@ function Calculadora() {
 
   this.clear = () => (this.display.value = "");
 
-  this.inicia = () => this.capturaClick();
-
   this.del = () => (this.display.value = this.display.value.slice(0, -1));
+
+  this.resultado = () => {
+    try {
+      const conta = eval(this.display.value);
+      if (!conta) {
+        alert("Conta inválida!");
+        return;
+      }
+
+      this.display.value = conta;
+    } catch (e) {
+      alert("Conta inválida!");
+      return;
+    }
+  };
 }
 
 const calculadora = new Calculadora();

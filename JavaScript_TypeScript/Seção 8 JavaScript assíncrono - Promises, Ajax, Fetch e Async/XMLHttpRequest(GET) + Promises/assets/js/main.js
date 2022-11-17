e9548b1909ea1a8fe01b3671189ebe8dc1793adc
José Carlos <etijcvivi@gmail.com>
2022-11-16 21:26:44 -0300

    JavaScript - TypeScript - Jose Carlos: Requisições Ajax

--- JavaScript_TypeScript/Seção 8 JavaScript assíncrono - Promises, Ajax, Fetch e Async/XMLHttpRequest(GET) + Promises/assets/js/main.js
+++ JavaScript_TypeScript/Seção 8 JavaScript assíncrono - Promises, Ajax, Fetch e Async/XMLHttpRequest(GET) + Promises/assets/js/main.js
@@ -1,4 +1,23 @@
+// Observação: xhr = XML Http Request padrão utilizado entre os desenvolvedores.
 const request = (obj) => {
   const xhr = new XMLHttpRequest();
   xhr.open(obj.method, obj.url, true);
+  xhr.send();
+
+  xhr.addEventListener("load", () => {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      obj.success(xhr.responseText);
+    } else {
+      obj.error(xhr.statusText);
+      //   obj.error({
+      //     code: xhr.status,
+      //     msg: xhr.statusText,
+      //   });
+    }
+  });
 };
+
+document.addEventListener("click", (e) => {
+  const el = e.target;
+  const tag = el.tagName.toLowerCase();
+});

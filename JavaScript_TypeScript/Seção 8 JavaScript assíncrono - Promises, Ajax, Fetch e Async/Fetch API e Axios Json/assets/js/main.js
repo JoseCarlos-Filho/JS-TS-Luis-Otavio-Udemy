8c43213e942a0ef36ea27d922a2d9cd25778cd49
José Carlos <etijcvivi@gmail.com>
2022-11-29 23:18:10 -0300

    JavaScript - TypeScript - Jose Carlos: FecthApi e Axios JSON

--- JavaScript_TypeScript/Seção 8 JavaScript assíncrono - Promises, Ajax, Fetch e Async/Fetch API e Axios Json/assets/js/main.js
+++ JavaScript_TypeScript/Seção 8 JavaScript assíncrono - Promises, Ajax, Fetch e Async/Fetch API e Axios Json/assets/js/main.js
@@ -1,3 +1,38 @@
 fetch("pessoas.json")
+  // primeiro then converte em json
   .then((resposta) => resposta.json())
-  .then((json) => console.log(json));
+  .then((json) => carregaElementos(json));
+
+function carregaElementos(json) {
+  const table = criaTable("table");
+  for (let pessoa of json) {
+    const tr = criaTable("tr");
+
+    let td = criaTable("td");
+    td.innerHTML = pessoa.nome;
+    tr.appendChild(td);
+
+    td = criaTable("td");
+    td.innerHTML = pessoa.idade;
+    tr.appendChild(td);
+
+    td = criaTable("td");
+    td.innerHTML = pessoa.salario;
+    tr.appendChild(td);
+
+    td = criaTable("td");
+    td.innerHTML = pessoa.email;
+    tr.appendChild(td);
+
+    table.appendChild(tr);
+  }
+  console.log(table);
+  const resultado = document.querySelector(".resposta");
+  resultado.appendChild(table);
+}
+
+function criaTable(elemento) {
+  if (elemento == "table") return (elemento = document.createElement("table"));
+  if (elemento == "tr") return (elemento = document.createElement("tr"));
+  if (elemento == "td") return (elemento = document.createElement("td"));
+}

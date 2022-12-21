310871711b55267b35ac6325451acbddbc8f1968
José Carlos <etijcvivi@gmail.com>
2022-12-21 18:37:19 -0300

    JavaScript TypeScript José Carlos: solucionando problemas de navegadores antigos core-js e regenarator-runtime.

---
+++ JavaScript_TypeScript/Seção 9 JavaScript tooling e ES6/core-js generator-runtime/modeloWebpack/src/main.js
@@ -0,0 +1,11 @@
+// corrigindo problemas de navegadores mais antigos
+// importanto o "core-js/stable" e o "regenerator-runtime/runtime"
+// import "core-js/stable";
+
+// import "regenerator-runtime/runtime";
+
+import executa from "./modules/promises";
+
+import "./assets/CSS/style.css";
+
+executa();

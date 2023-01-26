d6b04a1893666d07dd5ceb0b15cb58359dbea08c
José Carlos <etijcvivi@gmail.com>
2023-01-26 20:44:36 -0300

    Refactor: adicionando return nas funções de controller

--- JavaScript_TypeScript/Node/Express/src/controller/homeController.js
+++ JavaScript_TypeScript/Node/Express/src/controller/homeController.js
@@ -1,7 +1,10 @@
 exports.paginaInicial = (req, res) => {
   res.render("index");
+  return;
 };
 
 exports.trataPost = (req, res) => {
-  res.send(`Ei, sou sua nova rota de POST.`);
+  // res.send(`Ei, sou sua nova rota de POST.`);
+  res.send(req.body);
+  return;
 };

6b0a4e26d8605c43987e0d9d22a355740c49954d
José Carlos <etijcvivi@gmail.com>
2023-01-30 23:14:05 -0300

    feat: logando mensagem de rota post

--- JavaScript_TypeScript/Node/Express/src/controller/homeController.js
+++ JavaScript_TypeScript/Node/Express/src/controller/homeController.js
@@ -1,10 +1,12 @@
 exports.paginaInicial = (req, res) => {
+  console.log("Inicio");
   res.render("index");
   return;
 };
 
 exports.trataPost = (req, res) => {
-  // res.send(`Ei, sou sua nova rota de POST.`);
+  res.send(`Ei, sou sua nova rota de POST.`);
+  console.log("rota post");
   res.send(req.body);
   return;
 };

0a162b63f7eb83bdc244a5dd1362a6bb62608fd2
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-11-25 18:26:07 -0300

    feat: exportar middleware global para melhor organização do código

--- JavaScript_TypeScript/Node/Express/src/middlewares/middleware.js
+++ JavaScript_TypeScript/Node/Express/src/middlewares/middleware.js
@@ -1,5 +1,5 @@
 const { log } = console;
-module.exports = (req, res, next) => {
+exports.middlewareGlobal = (req, res, next) => {
   if (req.body.nome) {
     log();
     log(`Teste Post realizado com dados: ${req.body.cliente}`);

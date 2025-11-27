28d1f92b002481409e330b0d39c790176eae2281
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-11-26 22:05:51 -0300

    feat: remover código comentado de exemplo no homeController

--- JavaScript_TypeScript/Node/Express/src/controller/homeController.js
+++ JavaScript_TypeScript/Node/Express/src/controller/homeController.js
@@ -1,3 +1,12 @@
+// const HomeModel = require("../models/HomeModel");
+
+// HomeModel.create({
+//   titulo: "Outra coisa de teste",
+//   descricao: "Outra descricao de teste",
+// })
+//   .then((dados) => console.log(dados))
+//   .catch((e) => console.log(e));
+
 exports.paginaInicial = (req, res) => {
   console.log("Inicio");
   res.render("index");

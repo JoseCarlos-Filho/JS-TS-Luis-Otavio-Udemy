72e15c3197a20f7ccd2b195d937c50144cc702fd
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-12-04 20:42:27 -0300

    feat: adicionar variável local no middleware global

--- JavaScript_TypeScript/Node/Express/src/middlewares/middleware.js
+++ JavaScript_TypeScript/Node/Express/src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 const { log } = console;
 exports.middlewareGlobal = (req, res, next) => {
+  res.locals.umaVariavelLocal = "Este é o valor da variável local.";
   if (req.body.nome) {
     log();
     log(`Teste Post realizado com dados: ${req.body.cliente}`);
